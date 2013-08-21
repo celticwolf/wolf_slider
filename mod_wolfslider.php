0e13f2b255ece570ebd1753a84b7e3eba0adc1ba
Celtic Wolf, Inc <info@celticwolf.com>
2013-08-21 18:52:55 -0400

    Updated copyright and header info.

--- mod_wolfslider.php
+++ mod_wolfslider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 
+ *
  * WolfSlider is a free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
@@ -14,11 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *@author LaunchTulsa.com
- *@copyright (C) 2008 LaunchTulsa.com, 2010-2011 Celtic Wolf, Inc.
- *@link http://launchtulsa.com Official website
- *
- * Portions copyright 2010 Celtic Wolf, Inc.
- * http://www.celticwolf.com/
+ *@copyright (C) 2008 LaunchTulsa.com, 2010-2011, 2013 Celtic Wolf, Inc.
+ *@link http://www.celticwolf.com/
 **/
 
 // no direct access
